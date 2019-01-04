339525d584afa72c90a0a69dd3c836bba6ce427d
kowalski26 <prisonsona@gmail.com>
2019-01-04 16:43:51 +0100

    Add js file

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+
+$("#menu-item-1713").click(function (e){
+$('html, body').animate({
+scrollTop: $("#about-us").offset().top
+}, 500);
+e.preventDefault();
+});
+
+$("#menu-item-1715").click(function (e){
+$('html, body').animate({
+scrollTop: $("#offer").offset().top
+}, 1500);
+e.preventDefault();
+});
+$("#menu-item-1714").click(function (e){
+$('html, body').animate({
+scrollTop: $("#mission").offset().top
+}, 700);
+e.preventDefault();
+});
+
+
+$("#menu-item-1716").click(function (e){
+$('html, body').animate({
+scrollTop: $("#contact").offset().top
+}, 2000);
+e.preventDefault();
+});
+
+
+$("#menu-item-1718").click(function (e){
+$('html, body').animate({
+scrollTop: $("#about-us").offset().top
+}, 500);
+e.preventDefault();
+});
+
+$("#menu-item-1720").click(function (e){
+$('html, body').animate({
+scrollTop: $("#offer").offset().top
+}, 1500);
+e.preventDefault();
+});
+$("#menu-item-1719").click(function (e){
+$('html, body').animate({
+scrollTop: $("#mission").offset().top
+}, 700);
+e.preventDefault();
+});
+
+
+$("#menu-item-1721").click(function (e){
+$('html, body').animate({
+scrollTop: $("#contact").offset().top
+}, 2000);
+e.preventDefault();
+});
+
+
+$("#menu-item-1723").click(function (e){
+$('html, body').animate({
+scrollTop: $("#about-us").offset().top
+}, 500);
+e.preventDefault();
+});
+
+$("#menu-item-1725").click(function (e){
+$('html, body').animate({
+scrollTop: $("#offer").offset().top
+}, 1500);
+e.preventDefault();
+});
+$("#menu-item-1724").click(function (e){
+$('html, body').animate({
+scrollTop: $("#mission").offset().top
+}, 700);
+e.preventDefault();
+});
+
+
+$("#menu-item-1725").click(function (e){
+$('html, body').animate({
+scrollTop: $("#contact").offset().top
+}, 2000);
+e.preventDefault();
+});
+
+
+
